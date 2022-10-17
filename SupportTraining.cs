fb75b023d519ae904234618b5156a059d9ce06e0
Aaron-Morgan <morganac@bellaliant.net>
2022-10-17 17:42:25 -0230

    first commit

---
+++ SupportTraining.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i < 100; i++)
+        {
+            if (i % 3 == 0 & i % 5 == 0)
+            {
+                Console.WriteLine("SupportTraining");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Support");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Training");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
